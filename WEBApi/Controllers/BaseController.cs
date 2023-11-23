7db57255142c19a9c528cfe05c9e414b2978193a
Ebru Yüksek <ebru.yuksek@rempeople.com>
2023-11-23 17:24:22 +0300

    Add project files.

---
+++ WEBApi/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEBApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private IMediator? _mediator;
+        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        //sadece inherit edenler kullanabilsin diye protected yazıldı. 
+    }
+}
---
+++ WEBApi/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Features.Brands.Commands.Create;
+using MediatR;
+
+namespace WEBApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+    }
+}
